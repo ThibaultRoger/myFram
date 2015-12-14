627a9e465558eeb72a7074fd9cb1432dd7bc17b3
ThibaultRoger <thibaultroger1@gmail.com>
2015-12-14 18:45:41 +0100

    add myFram

---
+++ App/Layouts/layout.php
@@ -0,0 +1,33 @@
+<?php
+Namespace Layouts
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+   
+</head>
+
+<body>
+ 
+                <?php echo $this->Session->flash(); ?>
+        	<?php echo $content_for_layout; ?>
+
+              
+                
+          
+</body>
+
+</html>
+
+
+
+
+
+
+
+
+
+
